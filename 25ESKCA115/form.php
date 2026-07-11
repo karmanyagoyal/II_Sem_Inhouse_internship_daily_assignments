166cd1ca8967df7cfad4be7856922c674b48acf8
karmanyagoyal <karmanyagoyal07@gmail.com>
2026-07-11 22:55:42 +0530

    DAY 7-FORM

---
+++ 25ESKCA115/form.php
@@ -0,0 +1,13 @@
+<html>
+<form action ="registration.php" method="get">
+      <input type="text" name="name" placeholder="Enter your name">
+      <input type="email" name="email" placeholder="Enter your email">
+      <input type="text" name="branch" placeholder="Enter your branch">
+      <input type="text" name="phonenumber" placeholder="Enter your phone number">
+
+      <button type="submit">
+       Register
+      </button>
+
+</form>
+</html>
