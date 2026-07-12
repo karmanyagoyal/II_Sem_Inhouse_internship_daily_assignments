d3e4ea92f301ac215f7aa428a138c5a4c4273080
karmanyagoyal <karmanyagoyal07@gmail.com>
2026-07-12 07:16:55 +0530

    DAY 7-Login page

---
+++ 25ESKCA115/login.php
@@ -0,0 +1,20 @@
+<!-- login.php -->
+<form method="POST" action="login.php">
+<div class="mb-3">
+<label>Email Address</label>
+<input type="email" name="email"
+class="form-control"
+placeholder="you@example.com">
+
+</div>
+<div class="mb-3">
+<label>Password</label>
+<input type="password" name="password"
+class="form-control">
+
+</div>
+<button type="submit"
+class="btn btn-primary w-100">
+Login
+</button>
+</form>
